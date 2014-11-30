db93753fd1bfd2f250ad008d52da8427b0d43c64
TobiasKl <tobiasklaiber@gmx.de>
2014-11-30 21:42:11 +0100

    added another comment (this time on dev branch)

--- src/de/uniHannover/Remote/RemoteClass.java
+++ src/de/uniHannover/Remote/RemoteClass.java
@@ -5,6 +5,7 @@ public class RemoteClass {
     public static void main(String[] args) {
 	System.out.println("Remote project");
 	System.out.println("First change local");
+	System.out.println("First change on dev branch");
 
     }
 
